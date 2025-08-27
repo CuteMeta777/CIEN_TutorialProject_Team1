1b77ca44a663cd92b7ace2cfc9de345537b748bd
NataliatheAssassin <eigensoyul@gmail.com>
2025-08-27 21:03:30 +0900

    almost final build...?

--- Assets/Code/Manager/Temporal/CutsceneManager.cs
+++ Assets/Code/Manager/Temporal/CutsceneManager.cs
@@ -23,9 +23,9 @@ public class CutsceneManager : MonoBehaviour
     {
         for (int i = 0; i < cutscenes.Length; i++)
         {
-            for (float j = 0f; j <= 1f; j += 0.03125f) { cutscene_black_cover.color = new Color(0f, 0f, 0f, j); yield return null; }
+            for (float j = 0f; j <= 1f; j += 0.0078125f) { cutscene_black_cover.color = new Color(0f, 0f, 0f, j); yield return null; }
             image.sprite = cutscenes[i];
-            for (float j = 1f; j >= 0f; j -= 0.03125f) { cutscene_black_cover.color = new Color(0f, 0f, 0f, j); yield return null; }
+            for (float j = 1f; j >= 0f; j -= 0.0078125f) { cutscene_black_cover.color = new Color(0f, 0f, 0f, j); yield return null; }
             yield return new WaitForSeconds(2f);
         }
         CustomSceneManager.instance.GoToNextScene();
